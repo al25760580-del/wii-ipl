154826f9152e4f40a98fb5e27ba443ecb8951b9e
Tim Schneeberger <tim.schneeberger@outlook.de>
2026-08-11 04:07:22 +0200

    100% match MSL (except time.c is equivalent)

--- libs/MSL/include/ansi_fp.h
+++ libs/MSL/include/ansi_fp.h
@@ -1,29 +1,29 @@
 #ifndef ANSI_FP
 #define ANSI_FP
 
-#pragma options align=native
+#pragma options align = native
 
 typedef struct decimal {
-    char sign;
-    char _1;
-    short exponent;
+    char sign;       // 0x00
+    char _1;         // 0x01
+    short exponent;  // 0x02
 
     struct {
-        unsigned char length;
-        unsigned char text[36];
-        unsigned char pad;
-    } sig;
+        unsigned char length;    // 0x04
+        unsigned char text[36];  // 0x05
+        unsigned char pad;       // 0x29
+    } sig;                       // 0x04
 } decimal;
 
 typedef struct decform {
-    char style;
-    char _1;
-    short digits;
+    char style;    // 0x00
+    char _1;       // 0x01
+    short digits;  // 0x02
 } decform;
 
-void __num2dec(const decform *, double, decimal *);
-double __dec2num(const decimal *);
+void __num2dec(const decform*, double, decimal*);
+double __dec2num(const decimal*);
 
-#pragma options align=reset
+#pragma options align = reset
 
-#endif // ANSI_FP
+#endif  // ANSI_FP
--- libs/MSL/src/MSL_Common_Embedded/ansi_fp.c
+++ libs/MSL/src/MSL_Common_Embedded/ansi_fp.c
@@ -1,5 +1,4 @@
 #include "ansi_fp.h"
-#pragma exceptions on
 
 #include "internal/math_api.h"
 
@@ -10,10 +9,10 @@ extern double pow(double, double);
 
 /* builtin for __num2dec_internal*/
 inline int count_trailing(double x) {
-    return __builtin___count_trailing_zero64(*(unsigned long long *)&x | 0x0010000000000000);
+    return __builtin___count_trailing_zero64(*(unsigned long long*)&x | 0x0010000000000000);
 }
 
-static int __must_round(const decimal *pDecimal, int position) {
+static int __must_round(const decimal* pDecimal, int position) {
     unsigned char const* i = pDecimal->sig.text + position;
 
     if (*i > 5) {
@@ -42,9 +41,9 @@ static int __must_round(const decimal *pDecimal, int position) {
 }
 
 /* inlined in some spots */
-static void __dorounddecup(decimal *pDecimal, int digits) {
-    unsigned char *b = pDecimal->sig.text;
-    unsigned char *i = b + digits - 1;
+static void __dorounddecup(decimal* pDecimal, int digits) {
+    unsigned char* b = pDecimal->sig.text;
+    unsigned char* i = b + digits - 1;
 
     while (1) {
         if (*i < 9) {
@@ -57,12 +56,12 @@ static void __dorounddecup(decimal *pDecimal, int digits) {
             pDecimal->exponent++;
             break;
         }
-        
+
         *i-- = 0;
     }
 }
 
-static void __rounddec(decimal *pDecimal, int dig) {
+static void __rounddec(decimal* pDecimal, int dig) {
     if (dig <= 0 || dig >= pDecimal->sig.length) {
         return;
     }
@@ -79,7 +78,7 @@ static void __rounddec(decimal *pDecimal, int dig) {
     __dorounddecup(pDecimal, dig);
 }
 
-void __ull2dec(decimal *pDecimal, unsigned long long input) {
+static void __ull2dec(decimal* pDecimal, unsigned long long input) {
     pDecimal->sign = 0;
     pDecimal->sig.length = 0;
 
@@ -102,12 +101,12 @@ void __ull2dec(decimal *pDecimal, unsigned long long input) {
     pDecimal->exponent = (short)(pDecimal->sig.length - 1);
 }
 
-void __timesdec(decimal *pResult, const decimal *x, const decimal *y) {
+static void __timesdec(decimal* pResult, const decimal* x, const decimal* y) {
     unsigned long accumulator = 0;
     unsigned char mant[72];
     int i = x->sig.length + y->sig.length - 1;
-    unsigned char *ip = mant + i + 1;
-    unsigned char *ep = ip;
+    unsigned char* ip = mant + i + 1;
+    unsigned char* ep = ip;
     pResult->sign = 0;
 
     for (; i > 0; --i) {
@@ -116,17 +115,17 @@ void __timesdec(decimal *pResult, const decimal *x, const decimal *y) {
         int l;
         int t;
         unsigned char const *jp, *kp;
-        
+
         if (j < 0) {
             j = 0;
             k = i - 1;
         }
-        
+
         jp = x->sig.text + j;
         kp = y->sig.text + k;
         l = k + 1;
         t = x->sig.length - j;
-        
+
         if (l > t) {
             l = t;
         }
@@ -134,7 +133,7 @@ void __timesdec(decimal *pResult, const decimal *x, const decimal *y) {
         for (; l > 0; --l, ++jp, --kp) {
             accumulator += (unsigned long)(*jp) * *kp;
         }
-        
+
         *--ip = (unsigned char)(accumulator % 10);
         accumulator /= 10;
     }
@@ -145,7 +144,7 @@ void __timesdec(decimal *pResult, const decimal *x, const decimal *y) {
         *--ip = (unsigned char)accumulator;
         pResult->exponent++;
     }
-    
+
     for (i = 0; i < 36 && ip < ep; ++i, ++ip) {
         pResult->sig.text[i] = *ip;
     }
@@ -160,18 +159,18 @@ void __timesdec(decimal *pResult, const decimal *x, const decimal *y) {
                     goto round;
                 }
             }
-        
+
             if ((*(ip - 1) & 1) == 0) {
                 return;
             }
         }
-        round:
+    round:
         /* inlined */
         __dorounddecup(pResult, pResult->sig.length);
     }
 }
 
-void __str2dec(decimal *pDecimal, const char *pStr, short exp) {
+static void __str2dec(decimal* pDecimal, const char* pStr, short exp) {
     int i;
     pDecimal->exponent = exp;
     pDecimal->sign = 0;
@@ -204,12 +203,12 @@ void __str2dec(decimal *pDecimal, const char *pStr, short exp) {
             }
         }
 
-        round:
-            __dorounddecup(pDecimal, pDecimal->sig.length);
+    round:
+        __dorounddecup(pDecimal, pDecimal->sig.length);
     }
 }
 
-static void __two_exp(decimal *pDecimal, long exp) {
+static void __two_exp(decimal* pDecimal, long exp) {
     switch (exp) {
         case -64:
             __str2dec(pDecimal, "542101086242752217003726400434970855712890625", -20);
@@ -286,8 +285,7 @@ static void __two_exp(decimal *pDecimal, long exp) {
 
             if (exp > 0) {
                 __str2dec(&dec, "2", 0);
-            }
-            else {
+            } else {
                 __str2dec(&dec, "5", -1);
             }
 
@@ -296,12 +294,12 @@ static void __two_exp(decimal *pDecimal, long exp) {
     }
 }
 
-int __equals_dec(const decimal *x, const decimal *y) {
+static int __equals_dec(const decimal* x, const decimal* y) {
     if (x->sig.text[0] == 0) {
         if (y->sig.text[0] == 0) {
             return 1;
         }
-        
+
         return 0;
     }
 
@@ -316,7 +314,7 @@ int __equals_dec(const decimal *x, const decimal *y) {
     if (x->exponent == y->exponent) {
         int i;
         int l = x->sig.length;
-        
+
         if (l > y->sig.length) {
             l = y->sig.length;
         }
@@ -343,7 +341,7 @@ int __equals_dec(const decimal *x, const decimal *y) {
     return 0;
 }
 
-static int __less_dec(const decimal *x, const decimal *y) {
+static int __less_dec(const decimal* x, const decimal* y) {
     if (x->sig.text[0] == 0) {
         if (y->sig.text[0] != 0) {
             return 1;
@@ -388,157 +386,157 @@ static int __less_dec(const decimal *x, const decimal *y) {
     return x->exponent < y->exponent;
 }
 
-void __minus_dec(decimal *z, const decimal *x, const decimal *y) {
-	int zlen, dexp;
-	unsigned char *ib, *i, *ie;
-	unsigned char const *jb, *j, *jn;
-	*z = *x;
+static void __minus_dec(decimal* z, const decimal* x, const decimal* y) {
+    int zlen, dexp;
+    unsigned char *ib, *i, *ie;
+    unsigned char const *jb, *j, *jn;
+    *z = *x;
 
-	if (y->sig.text[0] == 0) {
-		return;
+    if (y->sig.text[0] == 0) {
+        return;
     }
 
-	zlen = z->sig.length;
+    zlen = z->sig.length;
 
-	if (zlen < y->sig.length) {
-		zlen = y->sig.length;
+    if (zlen < y->sig.length) {
+        zlen = y->sig.length;
     }
 
-	dexp = z->exponent - y->exponent;
-	zlen += dexp;
+    dexp = z->exponent - y->exponent;
+    zlen += dexp;
 
-	if (zlen > 36) {
-		zlen = 36;
-    }
- 
-	while (z->sig.length < zlen) {
-		z->sig.text[z->sig.length++] = 0;
+    if (zlen > 36) {
+        zlen = 36;
     }
 
-	ib = z->sig.text;
-	i = ib + zlen;
-
-	if (y->sig.length + dexp < zlen) {
-		i = ib + (y->sig.length + dexp);
+    while (z->sig.length < zlen) {
+        z->sig.text[z->sig.length++] = 0;
     }
 
-	jb = y->sig.text;
-	j = jb + (i - ib - dexp);
-	jn = j;
+    ib = z->sig.text;
+    i = ib + zlen;
 
-	while (i > ib && j > jb) {
-		--i;
-		--j;
-		if (*i < *j) {
-			unsigned char *k = i - 1;
-			while (*k == 0) {
-				--k;
+    if (y->sig.length + dexp < zlen) {
+        i = ib + (y->sig.length + dexp);
+    }
+
+    jb = y->sig.text;
+    j = jb + (i - ib - dexp);
+    jn = j;
+
+    while (i > ib && j > jb) {
+        --i;
+        --j;
+        if (*i < *j) {
+            unsigned char* k = i - 1;
+            while (*k == 0) {
+                --k;
             }
-			while (k != i) {
-				--*k;
-				*++k += 10;
-			}
-		}
-
-		*i -= *j;
-	}
-
-	if (jn - jb < y->sig.length) {
-		bool round_down = false;
-		if (*jn < 5) {
-			round_down = true;
+            while (k != i) {
+                --*k;
+                *++k += 10;
+            }
         }
-		else if (*jn == 5) {
-			unsigned char const *je = y->sig.text + y->sig.length;
-    
-			for (j = jn + 1; j < je; ++j) {
-				if (*j != 0) {
-					goto done;
+
+        *i -= *j;
+    }
+
+    if (jn - jb < y->sig.length) {
+        bool round_down = false;
+        if (*jn < 5) {
+            round_down = true;
+        } else if (*jn == 5) {
+            unsigned char const* je = y->sig.text + y->sig.length;
+
+            for (j = jn + 1; j < je; ++j) {
+                if (*j != 0) {
+                    goto done;
                 }
             }
 
-			i = ib + (jn - jb) + dexp - 1;
+            i = ib + (jn - jb) + dexp - 1;
 
-			if (*i & 1) {
-				round_down = 1;
+            if (*i & 1) {
+                round_down = 1;
             }
-		}
-		if (round_down) {
-			if (*i < 1) {
-				unsigned char *k = i - 1;
+        }
+        if (round_down) {
+            if (*i < 1) {
+                unsigned char* k = i - 1;
 
-				while (*k == 0) {
-					--k;
+                while (*k == 0) {
+                    --k;
                 }
-				while (k != i) {
-					--*k;
-					*++k += 10;
-				}
-			}
+                while (k != i) {
+                    --*k;
+                    *++k += 10;
+                }
+            }
 
-			*i -= 1;
-		}
-	}
+            *i -= 1;
+        }
+    }
 
 done:
-	for (i = ib; *i == 0; ++i);
+    for (i = ib; *i == 0; ++i)
+        ;
 
-	if (i > ib) {
-		unsigned char dl = (unsigned char)(i - ib);
-		z->exponent -= dl;
-		ie = ib + z->sig.length;
-		for (; i < ie; ++i, ++ib)
-			*ib = *i;
-		z->sig.length -= dl;
-	}
-
-	ib = z->sig.text;
-
-	for (i = ib + z->sig.length; i > ib;) {
-		--i;
-		if (*i != 0) {
-			break;
-        }
-	}
-
-	z->sig.length = (unsigned char)(i - ib + 1);
-}
-
-void __num2dec_internal(decimal *pDecimal, double x) {
-	char sign = (char)(__signbitd(x) != 0);
-	if (x == 0.0) {
-		pDecimal->sign = sign;
-		pDecimal->exponent = 0;
-		pDecimal->sig.length = 1;
-		pDecimal->sig.text[0] = 0;
-		return;
-	}
-
-	if (!(__fpclassifyd(x) > 2)) {
-		pDecimal->sign = sign;
-		pDecimal->exponent = 0;
-		pDecimal->sig.length = 1;
-		pDecimal->sig.text[0] = (unsigned char)(__fpclassifyd(x) == 1 ? 'N' : 'I');
-		return;
-	}
-
-	if (sign) {
-		x = -x;
+    if (i > ib) {
+        unsigned char dl = (unsigned char)(i - ib);
+        z->exponent -= dl;
+        ie = ib + z->sig.length;
+        for (; i < ie; ++i, ++ib)
+            *ib = *i;
+        z->sig.length -= dl;
     }
 
-	{
+    ib = z->sig.text;
+
+    for (i = ib + z->sig.length; i > ib;) {
+        --i;
+        if (*i != 0) {
+            break;
+        }
+    }
+
+    z->sig.length = (unsigned char)(i - ib + 1);
+}
+
+static void __num2dec_internal(decimal* pDecimal, double x) {
+    char sign = (char)(__signbitd(x) != 0);
+    if (x == 0.0) {
+        pDecimal->sign = sign;
+        pDecimal->exponent = 0;
+        pDecimal->sig.length = 1;
+        pDecimal->sig.text[0] = 0;
+        return;
+    }
+
+    if (!(__fpclassifyd(x) > 2)) {
+        pDecimal->sign = sign;
+        pDecimal->exponent = 0;
+        pDecimal->sig.length = 1;
+        pDecimal->sig.text[0] = (unsigned char)(__fpclassifyd(x) == 1 ? 'N' : 'I');
+        return;
+    }
+
+    if (sign) {
+        x = -x;
+    }
+
+    {
         int exp;
         double frac = frexp(x, &exp);
         long num_bits_extract = 53 - count_trailing(frac);
         decimal int_d, pow2_d;
-        __two_exp(&pow2_d, exp-num_bits_extract);
+        __two_exp(&pow2_d, exp - num_bits_extract);
         __ull2dec(&int_d, (unsigned long long)ldexp(frac, (int)num_bits_extract));
         __timesdec(pDecimal, &int_d, &pow2_d);
         pDecimal->sign = sign;
-	}
+    }
 }
 
-void __num2dec(const decform *pForm, double x, decimal *pDecimal) {
+void __num2dec(const decform* pForm, double x, decimal* pDecimal) {
     short digits = pForm->digits;
     int i;
 
@@ -565,34 +563,32 @@ void __num2dec(const decform *pForm, double x, decimal *pDecimal) {
     }
 }
 
-double __dec2num(const decimal *d)
-{
-	if (d->sig.length <= 0) {
-		return copysign(0.0, d->sign == 0 ? 1.0 : -1.0);
+double __dec2num(const decimal* d) {
+    if (d->sig.length <= 0) {
+        return copysign(0.0, d->sign == 0 ? 1.0 : -1.0);
     }
 
-	switch (d->sig.text[0]) {
-	case '0':
-		return copysign(0.0, d->sign == 0 ? 1.0 : -1.0);
-	case 'I':
-		return copysign((double)INFINITY, d->sign == 0 ? 1.0 : -1.0);
-	case 'N':
-		{
+    switch (d->sig.text[0]) {
+        case '0':
+            return copysign(0.0, d->sign == 0 ? 1.0 : -1.0);
+        case 'I':
+            return copysign((double)INFINITY, d->sign == 0 ? 1.0 : -1.0);
+        case 'N': {
             double result;
-            long long *ll = (long long*)&result;
+            long long* ll = (long long*)&result;
             *ll = 0x7FF0000000000000ULL;
             if (d->sign)
                 *ll |= 0x8000000000000000ULL;
             *ll |= 0x0008000000000000ULL;
-        
+
             return result;
-		}
-	}
-	{
+        }
+    }
+    {
         static double pow_10[8] = {1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8};
         decimal dec = *d;
-        unsigned char *i = dec.sig.text;
-        unsigned char *e = i + dec.sig.length;
+        unsigned char* i = dec.sig.text;
+        unsigned char* e = i + dec.sig.length;
         double first_guess;
         int exponent;
 
@@ -607,7 +603,7 @@ double __dec2num(const decimal *d)
             __str2dec(&max, "179769313486231580793728714053034151", 308);
             if (__less_dec(&max, &dec)) {
                 return copysign((double)INFINITY, d->sign == 0 ? 1.0 : -1.0);
-            }   
+            }
         }
 
         i = dec.sig.text;
@@ -629,7 +625,7 @@ double __dec2num(const decimal *d)
 
             temp1 = first_guess * pow_10[ndig - 1];
             temp2 = temp1 + ival;
-        
+
             if (ival != 0 && temp1 == temp2) {
                 break;
             }
@@ -639,8 +635,7 @@ double __dec2num(const decimal *d)
         }
         if (exponent < 0) {
             first_guess /= pow(5.0, -exponent);
-        }
-        else {
+        } else {
             first_guess *= pow(5.0, exponent);
         }
 
@@ -653,7 +648,7 @@ double __dec2num(const decimal *d)
         {
             decimal feedback1, feedback2, difflow, diffhigh;
             double next_guess;
-            long long *ull = (long long*)&next_guess;
+            long long* ull = (long long*)&next_guess;
             int guessed_low = 0;
             __num2dec_internal(&feedback1, first_guess);
 
@@ -672,19 +667,17 @@ double __dec2num(const decimal *d)
                     if (__fpclassifyd(next_guess) == 2) {
                         goto done;
                     }
-                }
-                else {
+                } else {
                     --*ull;
                 }
 
                 __num2dec_internal(&feedback2, next_guess);
                 if (guessed_low && !__less_dec(&feedback2, &dec)) {
                     break;
-                }
-                else if (!guessed_low && !__less_dec(&dec, &feedback2)) {
+                } else if (!guessed_low && !__less_dec(&dec, &feedback2)) {
                     difflow = feedback1;
                     feedback1 = feedback2;
-                    feedback2 =  difflow;
+                    feedback2 = difflow;
                     {
                         double temp = first_guess;
                         first_guess = next_guess;
@@ -702,15 +695,14 @@ double __dec2num(const decimal *d)
                 if (*(long long*)&first_guess & 1) {
                     first_guess = next_guess;
                 }
-            }
-            else if (!__less_dec(&difflow, &diffhigh)) {
+            } else if (!__less_dec(&difflow, &diffhigh)) {
                 first_guess = next_guess;
             }
         }
-        done:
-            if (dec.sign) {
-                first_guess = -first_guess;
-            }
-            return first_guess;
-	}
+    done:
+        if (dec.sign) {
+            first_guess = -first_guess;
+        }
+        return first_guess;
+    }
 }
